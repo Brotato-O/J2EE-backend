6a1c8a8231036b833e187aea2374ab94bebdb1f1
kaiST <phucnguyen17072005@gmail.com>
2026-09-19 13:02:29 +0700

    "feat: initialize Java project structure"

---
+++ project/src/test/java/J2EE/com/example/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package J2EE.com.example.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
